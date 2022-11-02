504b3e6dce1005994f34b949ea17468a16d9da45
kohyida1997 <kohyida1997@gmail.com>
2022-11-02 13:26:06 +0000

    Implement BasicArena Allocate, with Tests

--- allocator/include/shared/BasicArena.h
+++ allocator/include/shared/BasicArena.h
@@ -1,30 +1,22 @@
 #pragma once
 
+#include <algorithm>
 #include <cstddef>
 #include <iostream>
 #include <memory>
 
-namespace BasicArena {
-
-// CAP is the capacity in bytes of the arena
+namespace Arena {
+constexpr size_t max_alignment = alignof(std::max_align_t);
+// T is the capacity in bytes of the arena
 template <size_t T>
-// Basic Arena that allocates a memory pool on the Heap
+// Basic Arena that allocates a stack-like memory pool on the Heap,
+// monotonically. Pretty dumb yeah
 class BasicArena {
  public:
-  static constexpr size_t max_alignment = alignof(std::max_align_t);
-  BasicArena() {
+  BasicArena() noexcept {
     _buffer =
         new (std::align_val_t(max_alignment)) std::byte[T];  // On The Heap
     _ptr = _buffer;
-
-    // void* check = reinterpret_cast<void*>(_buffer);
-    // void* initial = check;
-    // size_t sz = T;
-    // void* newPtr = std::align(max_alignment, T, check, sz);
-    // std::cout << "Is Aligned? "
-    //           << (newPtr == initial ? "true" : "false")
-    //           << "\n";
-    // std::cout << "Adjusted " << newPtr << "\n";
   };
 
   ~BasicArena() {
@@ -32,14 +24,77 @@ class BasicArena {
     _ptr = nullptr;
     _buffer = nullptr;
   }
-  BasicArena(const BasicArena&) = delete;             // Delete copy constructor
+
+  // Cannot be copy-constructed, it represents a physical location on the RAM
+  BasicArena(const BasicArena&) = delete;  // Delete copy constructor
+
+  // Arena cannot be copy-assigned, it represents a physical location on the RAM
   BasicArena& operator=(const BasicArena&) = delete;  // Delete Copy Assignment
 
-  void* allocate(size_t s) { return nullptr; }
+  // Arena cannot be move-assigned, it represents a physical location on the RAM
+  BasicArena& operator=(BasicArena&&) = delete;  // Delete Move Assignment
+
+  // Arena cannot be move-constructed, it represents a physical location on the
+  // RAM
+  BasicArena(BasicArena&&) = delete;  // Delete Move constructor
+
+  void* allocate(size_t s, size_t align) {
+    if (available_size() == 0ul) {
+      std::cout << "Buffer available size is zero\n";
+      return reinterpret_cast<void*>(new std::byte[s]);
+    }
+
+    // Copy the pointer
+    auto* obj = reinterpret_cast<void*>(_ptr);
+    auto sz = available_size();
+    // Align it
+    obj = std::align(align, s, obj, sz);
+
+    // Fail to align/out of space, just malloc it
+    if (obj == nullptr) {
+      std::cout << "Align failed - nullptr, allocating on Heap instead\n";
+      return reinterpret_cast<void*>(new std::byte[s]);
+    }
+
+    // Cast to byte
+    auto* obj_byte_ptr = reinterpret_cast<std::byte*>(obj);
+
+    // This should be technically not necessary
+    if (!in_buffer(obj_byte_ptr + s - 1)) {
+      std::cout << "Ran out of space in buffer\n";
+      return reinterpret_cast<void*>(new std::byte[s]);
+    }
+
+    // Okay, advance the pointer
+    _ptr = obj_byte_ptr + s;
+    return obj_byte_ptr;
+  }
+
+  inline void* allocate(size_t s) noexcept {
+    return allocate(std::move(s), max_alignment);
+  }
+
+  // TODO: If ptr is at end of stack, move ptr back to reclaim space
+  void deallocate(std::byte* ptr, size_t x) noexcept {
+    if (!in_buffer(ptr)) delete ptr;
+  }
+
+  bool in_buffer(std::byte* test) noexcept {
+    return std::uintptr_t(test) < std::uintptr_t((_buffer + T)) &&
+           std::uintptr_t(test) >= std::uintptr_t(_buffer);
+  }
+
+  size_t capacity() { return T; }
+
+  size_t available_size() { return std::max(T - (_ptr - _buffer), 0uL); }
+
+  size_t used() { return _ptr - _buffer; }
 
  private:
+  // Pointer to start of the buffer
   std::byte* _buffer;
+  // Pointer to next available location in buffer
   std::byte* _ptr;
 };
 
-}  // namespace BasicArena
+}  // namespace Arena
--- allocator/src/main.cpp
+++ allocator/src/main.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <iostream>
 #include <memory>
 
@@ -13,20 +14,105 @@ void* operator new(size_t size) {
 
 void* operator new(std::size_t count, std::align_val_t al) {
   void* ptr = malloc(count);
-  std::cout << "Heap Alloc: " << count << " bytes at " << ptr
-            << " with align [" << static_cast<size_t>(al)  
-            << "]\n";
+  std::cout << "Heap Alloc: " << count << " bytes at " << ptr << " with align ["
+            << static_cast<size_t>(al) << "]\n";
   return ptr;
 }
 
-
 void operator delete(void* ptr) { std::cout << "Deleted " << ptr << std::endl; }
+
+void printHeader(const char* s) { std::cout << "\n=== " << s << " ===\n"; }
+
+// Test function prototypes
+void testAllocateSingleInt();
+void testAllocateFourCharMaxAlign();
+void testAllocateManyCharNaturalAlign();
+void testAllocateArenaTooSmall();
 
 int main() {
   // Say hi
   BasicAllocator::printBasicAllocatorHeader();
-
-  BasicArena::BasicArena<1024>();
-
+  testAllocateSingleInt();
+  testAllocateFourCharMaxAlign();
+  testAllocateManyCharNaturalAlign();
+  testAllocateArenaTooSmall();
   return 0;
+}
+
+void testAllocateSingleInt() {
+  printHeader(__func__);
+  auto arena = Arena::BasicArena<Arena::max_alignment>();
+  int* intPtrX = new (arena.allocate(sizeof(int))) int(25);
+  assert(intPtrX != nullptr);
+  assert(*intPtrX == 25);
+}
+
+void testAllocateFourCharMaxAlign() {
+  printHeader(__func__);
+  constexpr size_t sz = Arena::max_alignment * 4;
+  auto arena = Arena::BasicArena<sz>();
+  for (int i = 0; i < 4; i++) {
+    char* charPtr = new (arena.allocate(sizeof(char))) char('c');
+    assert(charPtr != nullptr);
+    assert(*charPtr == 'c');
+  }
+  // Last char has 15 bytes after it
+  assert(arena.available_size() == (sz - 3 * Arena::max_alignment - 1));
+  assert(arena.used() == (sz - arena.available_size()));
+
+  std::cout << ">>> Expect to see Heap Alloc:\n";
+  char* charPtr = new (arena.allocate(sizeof(char))) char('d');
+  assert(charPtr != nullptr);
+  assert(*charPtr == 'd');
+  assert(!arena.in_buffer(reinterpret_cast<std::byte*>(charPtr)));
+  std::cout << ">>> Expect to see global delete called for charPtr["
+            << static_cast<void*>(charPtr) << "]\n";
+  arena.deallocate(reinterpret_cast<std::byte*>(charPtr), sizeof(char));
+}
+
+void testAllocateManyCharNaturalAlign() {
+  printHeader(__func__);
+  constexpr int count = 1024;
+  constexpr size_t sz = Arena::max_alignment * static_cast<size_t>(1024);
+  auto arena = Arena::BasicArena<sz>();
+  // Allocate with max align
+  for (int i = 0; i < count; i++) {
+    char* charPtr = new (arena.allocate(sizeof(char))) char('c');
+    assert(charPtr != nullptr);
+    assert(*charPtr == 'c');
+    assert(arena.in_buffer(reinterpret_cast<std::byte*>(charPtr)));
+  }
+
+  // Last char has 15 bytes after it
+  assert(arena.available_size() ==
+         (sz - (count - 1) * Arena::max_alignment - 1));
+  assert(arena.used() == (sz - arena.available_size()));
+
+  // Allocate remaining using natural align
+  for (int i = arena.available_size(); i > 0; i--) {
+    auto* ptr = new (arena.allocate(sizeof(char), alignof(char))) char(i);
+    assert(ptr != nullptr);
+    assert(*ptr == i);
+    assert(arena.in_buffer(reinterpret_cast<std::byte*>(ptr)));
+  }
+
+  // Assert everything used up
+  assert(arena.used() == arena.capacity() && arena.available_size() == 0);
+}
+
+void testAllocateArenaTooSmall() {
+  printHeader(__func__);
+  // Too Small even for int
+  constexpr size_t sz = sizeof(int) / 2;
+  auto arena = Arena::BasicArena<sz>();
+  // Try to allocate anyway
+  int* intPtr = new (arena.allocate(sizeof(int), alignof(int))) int(12345);
+  std::cout << ">>> Expect to see global delete called for intPtr["
+            << static_cast<void*>(intPtr) << "]\n";
+
+  // Should not exist in the arena
+  assert(!arena.in_buffer(reinterpret_cast<std::byte*>(intPtr)));
+  assert(arena.available_size() == sizeof(int) / 2);
+  assert(arena.used() == 0);
+  arena.deallocate(reinterpret_cast<std::byte*>(intPtr), sizeof(int));
 }
